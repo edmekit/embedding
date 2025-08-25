fcbf57ccb985387fce55c69bdb692fd5f83786f7
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-08-25 22:02:07 +0800

    blink blink

--- proj1/main.c
+++ proj1/main.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+//blink built-in LED
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(115200);
   Serial.println("Hello, Philippines!");
 }
@@ -24,6 +25,8 @@ void loop() {
   
 }
 
+
+//blink LED using button
 void setup() {
   pinMode(2, OUTPUT);
   pinMode(4, INPUT_PULLUP);
@@ -38,4 +41,35 @@ void loop() {
   {
     digitalWrite(2, LOW);
   }
+}
+
+
+
+//Christmas lights
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Christmas Tree");
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+
+  delay(100);
+  digitalWrite(4, HIGH);
+  digitalWrite(2, LOW);
+
+  
+  delay(100);
+  digitalWrite(5, HIGH);
+  digitalWrite(4, LOW);
+
+  
+
+  delay(100);
+  digitalWrite(5, LOW);
+  
+
 }
